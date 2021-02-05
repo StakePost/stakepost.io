dd97616b31fbba1b5e83451998d92f657c2c2601
Andrew U <tmyx0m0p@gmail.com>
2021-02-05 15:20:00 +0300

    update config for react app

--- packages/frontend/src/app/config/index.jsx
+++ packages/frontend/src/app/config/index.jsx
@@ -1,11 +1,10 @@
 import development from "./development.config";
-import production from "./production.config";
 import staging from "./staging.config";
-import example from "./example.config";
-const env = process.env.NODE_ENV || "example";
+import production from "./production.config";
+
+const env = process.env.REACT_APP_ENV || "production";
 
 const config = {
-  example,
   development,
   production,
   staging,
